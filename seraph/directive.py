d78ddb11f2cdaf41c080513942ff72ae8ab30e80
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2023-03-17 13:33:47 -0500

    Update directive.py

--- seraph/directive.py
+++ seraph/directive.py
@@ -1,1 +1,11 @@
-class Directive:
+from seraph import utils
+
+class Directive(utils.DataContainer, utils.Makeable):
+	def __init__(self, *args, **kwargs):
+		utils.DataContainer.__init__(self, *args, **kwargs)
+		self._args = args
+		self._kwargs = kwargs
+		self.completed = False
+		
+	def duplicate(self) -> object:
+		return 
