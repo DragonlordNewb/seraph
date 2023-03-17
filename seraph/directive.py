697e507dfb3d9114c339f93f9a215e875bf89238
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2023-03-17 13:40:11 -0500

    Update directive.py

--- seraph/directive.py
+++ seraph/directive.py
@@ -1,7 +1,13 @@
 from seraph import utils
 
 class Directive(utils.DataContainer, utils.Makeable):
+	requires = {} # dict of {name: default}
+	
 	def __init__(self, *args, **kwargs):
+		for key in self.requires.keys():
+			if not key in kwargs.keys():
+				kwargs[key] = self.requires[key]
+				
 		utils.DataContainer.__init__(self, *args, **kwargs)
 		self._args = args
 		self._kwargs = kwargs
@@ -9,3 +15,6 @@ class Directive(utils.DataContainer, utils.Makeable):
 		
 	def duplicate(self) -> object:
 		return self.make(*self._args, **self._kwargs)
+	
+	def execute(self) -> bool:
+		raise SyntaxError("Can't execute a base Directive class - use a subclass.")
