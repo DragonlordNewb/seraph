62cb4d9f8e27a693ab70202153cdb3d46ffb6941
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2023-03-21 11:35:30 -0500

    Update common.py

--- seraph/common.py
+++ seraph/common.py
@@ -32,3 +32,28 @@ def levenshtein(a, b):
         levenshtein(a[1:], b),
         levenshtein(a[1:], b[1:])
     ])
+
+class ActivationFunction:
+	def __init__(self, activation: Union[utils.function, None]=None, derivative: Union[utils.function, None]=None) -> None:
+		if activation != None:
+			self.activation = activation
+		if derivative != None:
+			self.derivative = derivative
+		assert hasattr(self, "activation"), "Must subclass a \"activation(self, x)\" method onto ActivationFunction class."
+		assert hasattr(self, "derivative"), "Must subclass a \"derivative(self, x)\" method onto ActivationFunction class."
+		assert type(self(ACTIVATION, [1, 0])) in [int, float], "Activation function must return an int or float."
+		assert type(self(DERIVATIVE, [1, 0])) in [int, float], "Derivative function must return an int or float."
+		
+	def __repr__(self) -> str:
+		return "<seraph.ActivationFunction " + self.__name__ + ">"
+	
+	def __call__(self, mode: ACTIVATION or DERIVATIVE, x: Union[int, float]) -> Union[int, float]:
+		if mode == ACTIVATION:
+			if type(x) == list:
+				return [self(ACTIVATION, y) for y in x]
+			return self.activation(x)
+		elif mode == DERIVATIVE:
+			if type(x) == list:
+				return [self(DERIVATIVE, y) for y in x]
+			return self.derivative(x)
+		raise SyntaxError("\"mode\" argument of calling an ActivationFunction must be ACTIVATION or DERIVATIVE.")
