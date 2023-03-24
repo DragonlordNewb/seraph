55035bb825fdc52a0069cf601b90ec6f19598315
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2023-03-24 07:52:03 -0500

    Update language.py

--- seraph/language.py
+++ seraph/language.py
@@ -2,12 +2,20 @@
 # PUBLIC LICENSE ("AGREEMENT"). ANY USE, REPRODUCTION OR DISTRIBUTION
 # OF THE PROGRAM CONSTITUTES RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT.
 
-import nltk
+from seraph import entity
+from seraph import reaction
+from seraph import utils
 
-from nltk.corpus import wordnet
-from nltk.corpus import framenet
-from nltk.tag import pos_tag
-from nltk.tokenize import word_tokenize
-from nltk.tokenize import sent_tokenize
-from nltk.sentiment import vader
-from nltk.corpus import opinion_lexicon
+with utils.Indent():
+	print("Importing NLTK ...", end="")
+	import nltk
+	from nltk.corpus import wordnet
+	from nltk.corpus import framenet
+	from nltk.tag import pos_tag
+	from nltk.tokenize import word_tokenize
+	from nltk.tokenize import sent_tokenize
+	from nltk.sentiment import vader
+	from nltk.corpus import opinion_lexicon
+	print("done.")
+
+class Tokenization:
