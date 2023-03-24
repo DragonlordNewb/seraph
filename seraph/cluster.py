06900e13f2017c37041d895c5fa9e638d008c2f4
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2023-03-24 13:14:09 -0500

    commit

--- seraph/cluster.py
+++ seraph/cluster.py
@@ -1,5 +1,10 @@
+import random
+
 from seraph import entity as _entity
 from seraph import utils
+
+class OverextensionError(RuntimeError):
+	pass
 
 class Cluster:
 	def __init__(self, *entities: list[_entity.Entity]):
@@ -20,6 +25,12 @@ class Cluster:
 		if self.n >= len(self):
 			raise StopIteration
 		return self[self.n]
+
+	def __add__(self, other: object) -> object:
+		return Cluster(self.entities + other.entities)
+
+	def __mod__(self, other: object) -> int or float:
+		return sum([e1 % e2 for e1, e2 in zip(self, other)])
 		
 	def addEntity(self, entity: _entity.Entity) -> None:
 		self.entities.append(entity)
@@ -53,3 +64,46 @@ class Cluster:
 		thresholds.sort()
 		threshold = thresholds[n]
 		return self.pack(threshold, recursion)
+
+class ClusterTree:
+	def __init__(self, *entities: list[_entity.Entity]) -> None:
+		self.entities = entities
+		self.clusters = [Cluster(*[entity for entity in self.entities])]
+		self.layers = [self.clusters]
+
+	def __repr__(self) -> str:
+		return "<seraph.cluster.ClusterTree of length " + str(len(self)) + ">"
+
+	def __len__(self) -> int:
+		return len(self.entities)
+
+	def __getitem__(self, index: int) -> list[Cluster]:
+		return self.layers[index]
+
+	def _extendOnce(self) -> list[Cluster]:
+		if len(self.layers[-1]) > 2:
+			raise OverextensionError
+		clusters = self.layers[-1]
+
+		newClusters = []
+		while len(clusters) > 1:
+			current = clusters.pop(random.randint(0, len(clusters)))
+			clustersBySimilarity = {current % c: i for i, c in enumerate(clusters)}
+			highestSimilarity = max(clustersBySimilarity.keys())
+			newClusters.append(current + clusters.pop(clustersBySimilariy[highestSimilarity]))
+
+		newClusters.append(Cluster(clusters[0]))
+		
+		self.layers.append(newClusters)
+		return newClusters
+
+	def extend(self, n: int=1) -> list[Cluster]:
+		output = None
+		for _ in range(n):
+			try:
+				output = self._extendOnce()
+			except OverextensionError as e:
+				if output:
+					return output
+				raise e
+		return output
